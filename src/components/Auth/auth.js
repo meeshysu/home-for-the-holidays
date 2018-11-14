6e2e861076ef6f928d5b82fb6d8d81340781099f
Michelle Beshears <meeshysu@gmail.com>
2018-11-13 19:59:57 -0600

    new google button works for sign in

--- src/components/Auth/auth.js
+++ src/components/Auth/auth.js
@@ -3,9 +3,13 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import './auth.scss';
 
+import googleImage from './Sign-In.png';
+
 const loginButton = () => {
   const domString = `
-    <button id ="google-auth" class="btn btn-light">Login</button>
+    <button id ="google-auth" class="btn btn-light">
+      <img src="${googleImage}"/>
+    </button>
   `;
   $('#auth').html(domString);
   $('#google-auth').on('click', () => {
